cbabe936e5f1c4b9e0bf0d1586261df20ff590df
Ahmad <ahmadjarafat@gmail.com>
2023-07-07 22:47:48 +0300

    finish project setup

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+const Header = () => {
+  <Stack>
+    <Typography>CORA'L</Typography>
+  </Stack>;
+};
+
+export default Header;
---
+++ src/themes/theme.js
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1b4b66",
+      tint: "#639599",
+    },
+    error: {
+      main: "#b00020",
+    },
+    highlight: {
+      main: "#ff8c4b",
+    },
+    accent: {
+      main: "#f4f4f4",
+    },
+    dark: {
+      main: "#13101e",
+    },
+    bright: {
+      main: "#fff",
+    },
+    grey: {
+      main: "#f1f1f1",
+    },
+    lightText: {
+      main: "#b6b6b6",
+    },
+    highEmphasis: {
+      main: "#171520",
+    },
+    lowEmphasis: {
+      main: "#626262",
+    },
+  },
+});
+export default theme;
