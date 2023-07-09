a8cfaaa46cc007724b1e0a484c45dee1806303d2
Ahmad <ahmadjarafat@gmail.com>
2023-07-09 18:29:24 +0300

    final file structure and setup

--- src/components/links/primary-text-link/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Link } from "@mui/material";
-
-const PrimaryTextLink = ({ text = "primariy", path }) => {
-  return <Link color="primary">{text}</Link>;
-};
-
-export default PrimaryTextLink;
--- src/components/links/remove-text-link/index.jsx
+++ src/components/links/remove-text-link/index.jsx
@@ -1,16 +1,1 @@
-import { Link } from "@mui/material";
 
-const removeTextLink = (text = "remove", remove) => {
-  return (
-    <Link
-      //   onClick={() => {
-      //     remove();
-      //   }}
-      color="error"
-    >
-      {text}
-    </Link>
-  );
-};
-
-export default removeTextLink;
