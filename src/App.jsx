a555f872131c730c5d60b3ad48aa7bcb9e369b64
zaidzaidan <zaidzaidan79@gmail.com>
2023-07-11 15:21:00 +0300

    add the footer to the main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/header";
 import { ThemeProvider } from "@mui/material";
 import theme from "./themes/theme";
 import { BrowserRouter } from "react-router-dom";
+import Footer from './components/footer';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Header />
+        <Footer/>
       </ThemeProvider>
     </BrowserRouter>
   );
