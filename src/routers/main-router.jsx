a7823d19a957ae9e23d10e3025f8972530c52671
Ahmad <ahmadjarafat@gmail.com>
2023-07-20 09:53:01 +0300

    add deplyment settings

--- src/routers/main-router.jsx
+++ src/routers/main-router.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { BrowserRouter,Route,Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SharedLayout from "../components/shared-layout";
 import Home from "../pages/home";
 
 const LayOut = () => {
-
-
-  return(
-    <BrowserRouter>
-        <Routes>
-            <Route element={<SharedLayout/>}>
-                <Route path="/" element={<Home/>} />
-            </Route>
-        </Routes>
+  return (
+    <BrowserRouter basename="/frontend-final-2">
+      <Routes>
+        <Route element={<SharedLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default LayOut;
