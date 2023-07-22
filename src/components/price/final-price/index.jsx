9f89566e9c58c29efcf21fb7594f29c1c18a645b
Ahmad <ahmadjarafat@gmail.com>
2023-07-22 17:03:00 +0300

    add font weight prop to final price

--- src/components/price/final-price/index.jsx
+++ src/components/price/final-price/index.jsx
@@ -1,15 +1,20 @@
 import { Typography } from "@mui/material";
 import theme from "../../../themes/theme";
 
-const FinalPrice = ({fontSize="1rem", price, discount=1 }) => {
+const FinalPrice = ({
+  fontSize = "1rem",
+  fontWeight = "700",
+  price,
+  discount = 1,
+}) => {
   return (
     <Typography
       variant="h3"
       color={"highEmphasis"}
       fontSize={fontSize}
-      fontWeight={"700"}
+      fontWeight={fontWeight}
     >
-      ${discount*price}
+      ${discount * price}
     </Typography>
   );
 };
